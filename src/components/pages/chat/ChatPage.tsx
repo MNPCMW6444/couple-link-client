b02d27a154c6d8d7a1511d7eb4b06de34b1fb210
Michael Nusair <mnpcmw6444@gmail.com>
2023-10-30 18:16:19 +0200

    init

--- src/components/pages/chat/ChatPage.tsx
+++ src/components/pages/chat/ChatPage.tsx
@@ -17,6 +17,7 @@ const ChatPage: React.FC = () => {
     const {contactsIds} = useContext(ContactsContext);
     const [message, setMessage] = useState<string>("");
 
+    const myTurn = !(triplets[triplets.length - 1]?.me !== "" || (triplets[triplets.length - 1]?.me !== "" && triplets[triplets.length - 1]?.him !== "" && triplets[triplets.length - 1]?.ai !== ""));
 
     const {} = useContext(ChatContext);
 
@@ -46,9 +47,14 @@ const ChatPage: React.FC = () => {
             ) : null}
             {triplets.map((triplet, idx) => <ChatTriplet key={idx} triplet={triplet}/>)}
             <div style={{display: 'flex', alignItems: 'center', padding: '1em'}}>
-                <TextField value={message} onChange={(e) => setMessage(e.target.value)} variant="outlined" fullWidth
-                           placeholder="Type a message..."/>
-                <Button variant="contained" color="primary" onClick={() => {
+                <TextField
+                    disabled={!myTurn}
+                    value={myTurn ? message : "Wait for your turn...."} onChange={(e) => setMessage(e.target.value)}
+                    variant="outlined" fullWidth
+                    placeholder="Type a message..."/>
+                <Button
+                    disabled={!myTurn}
+                    variant="contained" color="primary" onClick={() => {
                     sendMessage(selectedSession, message);
                     setMessage("");
                 }}>
