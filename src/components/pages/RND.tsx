5518a7f2dab5bd6a2d426bd555ef52fee67d9a41
Michael Nusair <mnpcmw6444@gmail.com>
2023-11-10 21:24:14 +0200

    after sidebar cleanup

--- src/components/pages/RND.tsx
+++ src/components/pages/RND.tsx
@@ -1,13 +1,27 @@
 import {useState} from 'react';
 import {useQuery, useMutation, gql} from '@apollo/client';
-import {Grid, Typography, TextField, Button, List, ListItem, ListItemText, CircularProgress} from "@mui/material";
+import {
+    Grid,
+    Typography,
+    TextField,
+    Button,
+    List,
+    ListItem,
+    ListItemText,
+    CircularProgress
+} from "@mui/material";
 import useMobile from "../../hooks/responsiveness/useMobile.ts";
 
-const GET_ROLES = gql`
-  query Getroles {
-    getroles {
+const GET_MY_ROLES = gql`
+  query Getmyroles {
+    getmyroles {
+      creatorId
       role
-      example
+      messageOneExample
+      messageTwoExample
+      category
+      description
+      visibility
       _id
       createdAt
       updatedAt
@@ -16,56 +30,122 @@ const GET_ROLES = gql`
 `;
 
 const ADD_ROLE = gql`
-  mutation Addrole($role: String!) {
-    addrole(role: $role)
+  mutation Addrole($role: String!, $category: String!, $description: String!, $messageOneExample: String, $messageTwoExample: String) {
+    addrole(role: $role, category: $category, description: $description, messageOneExample: $messageOneExample, messageTwoExample: $messageTwoExample)
+  }
+`;
+
+const PUBLISH_ROLE = gql`
+  mutation Publishrole($roleId: String!) {
+    publishrole(roleId: $roleId)
   }
 `;
 
 const RND = () => {
     const {isMobile} = useMobile();
-    const {loading, error, data, refetch} = useQuery(GET_ROLES);
+    const {loading, error, data, refetch} = useQuery(GET_MY_ROLES);
     const [addRole] = useMutation(ADD_ROLE, {
-        refetchQueries: [GET_ROLES],
+        refetchQueries: [GET_MY_ROLES],
     });
+    const [publishRole] = useMutation(PUBLISH_ROLE);
     const [newRole, setNewRole] = useState('');
-    const [mutating, smutating] = useState(false);
+    const [category, setCategory] = useState('');
+    const [description, setDescription] = useState('');
+    const [messageOneExample, setMessageOneExample] = useState('');
+    const [messageTwoExample, setMessageTwoExample] = useState('');
+    const [mutating, setMutating] = useState(false);
 
     const handleAddRole = () => {
-        smutating(true)
-        addRole({variables: {role: newRole}}).then(() => {
-            refetch()
-            smutating(false)
-
-        })
+        setMutating(true);
+        addRole({
+            variables: {
+                role: newRole,
+                category,
+                description,
+                messageOneExample,
+                messageTwoExample
+            }
+        }).then(() => {
+            refetch();
+            setMutating(false);
+        });
         setNewRole('');
+        setCategory('');
+        setDescription('');
+        setMessageOneExample('');
+        setMessageTwoExample('');
+    };
+
+    const handlePublishRole = (roleId: string) => {
+        publishRole({variables: {roleId}});
     };
 
     if (loading) return <CircularProgress/>;
     if (error) return <p>Error :(</p>;
 
-    return <Grid container direction="column" padding="5%">
-        <Typography variant={isMobile ? "h3" : "h1"} align="center" gutterBottom>
-            Roles
-        </Typography>
-        <TextField
-            label="New Role"
-            value={newRole}
-            onChange={(e) => setNewRole(e.target.value)}
-            fullWidth
-            margin="normal"
-        />
-        <Button disabled={mutating} variant="contained" color="primary" onClick={handleAddRole}>
-            Add Role
-        </Button>
-        <List>
-            {data.getroles.map(({role, example, _id}: any) => (
-                <ListItem key={_id}>
-                    <ListItemText primary={role} secondary={example}/>
-                </ListItem>
-            ))}
-        </List>
-    </Grid>
-
+    return (
+        <Grid container direction="column" padding="5%">
+            <Typography variant={isMobile ? "h3" : "h1"} align="center" gutterBottom>
+                Roles
+            </Typography>
+            <TextField
+                label="New Role"
+                value={newRole}
+                onChange={(e) => setNewRole(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Message One Example"
+                value={messageOneExample}
+                onChange={(e) => setMessageOneExample(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <TextField
+                label="Message Two Example"
+                value={messageTwoExample}
+                onChange={(e) => setMessageTwoExample(e.target.value)}
+                fullWidth
+                margin="normal"
+            />
+            <Button disabled={mutating} variant="contained" color="primary" onClick={handleAddRole}>
+                Add Role
+            </Button>
+            <List>
+                {data.getmyroles.map(({
+                                          _id,
+                                          role,
+                                          category,
+                                          description,
+                                          messageOneExample,
+                                          messageTwoExample
+                                      }: any) => (
+                    <ListItem key={_id}>
+                        <ListItemText
+                            primary={`${role} (${category})`}
+                            secondary={`Description: ${description}, Examples: ${messageOneExample}, ${messageTwoExample}`}
+                        />
+                        <Button onClick={() => handlePublishRole(_id)}>Publish</Button>
+                    </ListItem>
+                ))}
+            </List>
+        </Grid>
+    );
 };
 
 export default RND;
