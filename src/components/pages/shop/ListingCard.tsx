fd8114759e3c22cbc5f9e84050d8bde8f22528c1
Michael Nusair <mnpcmw6444@gmail.com>
2023-12-01 20:29:02 +0200

    after sidebar cleanup

--- src/components/pages/shop/ListingCard.tsx
+++ src/components/pages/shop/ListingCard.tsx
@@ -2,14 +2,23 @@ import {Card, CardContent, Typography, Button, Modal} from '@mui/material';
 import {FC, useState} from "react";
 import FullCard from "./FullCard.tsx";
 import {Item} from "./ShopPage.tsx";
+import {gql, useMutation} from "@apollo/client";
 
 interface ListingCardProps {
     item: Item
 }
 
+const BUY_MUTATION = gql`
+    mutation Buy($id: String!) {
+      buy(id: $id)
+    }
+`
+
 const ListingCard: FC<ListingCardProps> = ({item}) => {
     const {name, category, description} = item
     const [open, setOpen] = useState(false);
+
+    const [buy] = useMutation(BUY_MUTATION)
 
     return (
         <Card>
@@ -18,6 +27,8 @@ const ListingCard: FC<ListingCardProps> = ({item}) => {
                 <Typography color="textSecondary">{category}</Typography>
                 <Typography variant="body2">{description}</Typography>
                 <Button onClick={() => setOpen(true)} variant="contained" color="primary">View More</Button>
+                <Button onClick={() => buy({variables: {id: item._id}})} variant="contained"
+                        color="primary">Buy</Button>
             </CardContent>
             <Modal open={open} onClose={() => setOpen(false)}>
                 <FullCard item={item}/>
