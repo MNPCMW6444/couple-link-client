ff29cdb7e7fd23e4cff5b11d840f2f0c0e1712f3
Michael Nusair <mnpcmw6444@gmail.com>
2023-12-06 12:41:42 +0200

    after sidebar cleanup

--- src/components/pages/settings/AccountTab.tsx
+++ src/components/pages/settings/AccountTab.tsx
@@ -5,6 +5,7 @@ import {gql, useMutation} from "@apollo/client";
 import UserContext from "../../../context/UserContext.tsx";
 import {useNavigate} from "react-router-dom";
 
+
 const StyledContainer = styled(Container)`
   display: flex;
   flex-direction: column;
@@ -16,6 +17,7 @@ const StyledContainer = styled(Container)`
 const StyledPaper = styled(Paper)`
   padding: 16px;
   width: 100%;
+  max-width: 600px;
 `;
 
 const UPDATE_RND_MUTATION = gql`
@@ -25,22 +27,18 @@ const UPDATE_RND_MUTATION = gql`
 `;
 
 const NotificationsTab: FC = () => {
-
     const {user, refreshUserData} = useContext(UserContext);
-
     const [rnd, setRnd] = useState(user.rnd);
-
     const [updateRND, {loading}] = useMutation(UPDATE_RND_MUTATION, {
         variables: {rnd: !rnd ? "true" : "false"},
         onCompleted: () => {
-            refreshUserData()
+            refreshUserData();
         },
         onError: () => {
-            refreshUserData()
+            refreshUserData();
         },
     });
 
-    // Handle switch change
     const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setRnd(event.target.checked);
         updateRND();
@@ -54,42 +52,61 @@ const NotificationsTab: FC = () => {
                 Account Settings
             </Typography>
             <StyledPaper>
-                <Grid container direction="column" rowSpacing={2}>
+                <Grid container wrap="nowrap" direction="column" spacing={2}>
                     <Grid item>
-                        <Typography variant="h6" gutterBottom>
-                            Phone Number: +{user.phone}
+                        <Typography variant="h6" gutterBottom component="div"
+                                    sx={{fontSize: "90%"}}>
+                            <strong>Phone Number:</strong> +{user.phone}
                         </Typography>
                     </Grid>
                     <Grid item>
-                        <Typography variant="h6" gutterBottom>
-                            Email Address: {user.email}
+                        <Typography variant="h6" gutterBottom component="div"
+                                    sx={{fontSize: "90%"}}>
+                            <strong>Email Address:</strong> {user.email}
                         </Typography>
                     </Grid>
-                    <Grid item container alignItems="center" columnSpacing={2}>
-                        <Grid item>
-                            <Typography variant="h6" gutterBottom>
-                                Subscription Status: {user.subscription === "free" ? "Inactive" : "Active"}
+                    <Grid item container wrap="nowrap" alignItems="center" spacing={3}>
+                        <Grid item xs={12} sm={5}>
+                            <Typography variant="h6" gutterBottom component="div"
+                                        sx={{fontSize: "90%"}}>
+                                <strong>Subscription Status:</strong>
                             </Typography>
                         </Grid>
-                        <Grid item>
-                            <Button
-                                variant="contained" onClick={() => navigate("/sub")}>
-                                Subscribe
+                        <Grid item xs={12} sm={2}>
+                            <Typography variant="body1" gutterBottom
+                                        sx={{fontSize: "90%"}}>
+                                {user.subscription === "free" ? "Inactive" : "Active"}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={5}>
+                            <Button sx={{fontSize: "60%"}} variant="contained" onClick={() => navigate("/sub")}>
+                                Manage Subscription
                             </Button>
                         </Grid>
                     </Grid>
-                    <Grid item>
-
-                        <FormControlLabel
-                            control={
-                                <Switch
-                                    checked={rnd}
-                                    onChange={handleSwitchChange}
-                                    disabled={loading}
-                                />
-                            }
-                            label="I want to develop and sell Roles"
-                        />
+                    <Grid item container wrap="nowrap" alignItems="center" spacing={3}>
+                        <Grid item xs={12} sm={6}>
+                            <Typography sx={{fontSize: "90%"}}
+                                        variant="h6" gutterBottom
+                                        component="div">
+                                <strong>R&D Mode:</strong>
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <FormControlLabel
+                                control={
+                                    <Switch
+                                        checked={rnd}
+                                        onChange={handleSwitchChange}
+                                        disabled={loading}
+                                    />
+                                }
+                                label={<Typography
+                                    sx={{fontSize: "90%"}}
+                                    variant="body1">Enable R&D
+                                    Mode</Typography>}
+                            />
+                        </Grid>
                     </Grid>
                 </Grid>
             </StyledPaper>
