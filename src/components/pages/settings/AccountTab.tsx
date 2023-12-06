d19b32983685b88e7b10f1ac97511ff2162a25c3
Michael Nusair <mnpcmw6444@gmail.com>
2023-12-06 12:45:23 +0200

    after sidebar cleanup

--- src/components/pages/settings/AccountTab.tsx
+++ src/components/pages/settings/AccountTab.tsx
@@ -5,7 +5,6 @@ import {gql, useMutation} from "@apollo/client";
 import UserContext from "../../../context/UserContext.tsx";
 import {useNavigate} from "react-router-dom";
 
-
 const StyledContainer = styled(Container)`
   display: flex;
   flex-direction: column;
@@ -30,13 +29,9 @@ const NotificationsTab: FC = () => {
     const {user, refreshUserData} = useContext(UserContext);
     const [rnd, setRnd] = useState(user.rnd);
     const [updateRND, {loading}] = useMutation(UPDATE_RND_MUTATION, {
-        variables: {rnd: !rnd ? "true" : "false"},
-        onCompleted: () => {
-            refreshUserData();
-        },
-        onError: () => {
-            refreshUserData();
-        },
+        variables: {rnd: rnd ? "false" : "true"},
+        onCompleted: () => refreshUserData(),
+        onError: () => refreshUserData(),
     });
 
     const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -47,48 +42,38 @@ const NotificationsTab: FC = () => {
     const navigate = useNavigate();
 
     return (
-        <StyledContainer maxWidth="xs">
+        <StyledContainer maxWidth="sm">
             <Typography variant="h4" gutterBottom>
                 Account Settings
             </Typography>
             <StyledPaper>
-                <Grid container wrap="nowrap" direction="column" spacing={2}>
+                <Grid container direction="column" spacing={2}>
                     <Grid item>
-                        <Typography variant="h6" gutterBottom component="div"
-                                    sx={{fontSize: "90%"}}>
+                        <Typography variant="subtitle1" gutterBottom>
                             <strong>Phone Number:</strong> +{user.phone}
                         </Typography>
                     </Grid>
                     <Grid item>
-                        <Typography variant="h6" gutterBottom component="div"
-                                    sx={{fontSize: "90%"}}>
+                        <Typography variant="subtitle1" gutterBottom>
                             <strong>Email Address:</strong> {user.email}
                         </Typography>
                     </Grid>
-                    <Grid item container wrap="nowrap" alignItems="center" spacing={3}>
-                        <Grid item xs={12} sm={5}>
-                            <Typography variant="h6" gutterBottom component="div"
-                                        sx={{fontSize: "90%"}}>
-                                <strong>Subscription Status:</strong>
+                    <Grid item container alignItems="center" spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <Typography variant="subtitle1" gutterBottom>
+                                <strong>Subscription
+                                    Status:</strong> {user.subscription === "free" ? "Inactive" : "Active"}
                             </Typography>
                         </Grid>
-                        <Grid item xs={12} sm={2}>
-                            <Typography variant="body1" gutterBottom
-                                        sx={{fontSize: "90%"}}>
-                                {user.subscription === "free" ? "Inactive" : "Active"}
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={5}>
-                            <Button sx={{fontSize: "60%"}} variant="contained" onClick={() => navigate("/sub")}>
+                        <Grid item xs={12} sm={6}>
+                            <Button variant="contained" onClick={() => navigate("/sub")}>
                                 Manage Subscription
                             </Button>
                         </Grid>
                     </Grid>
-                    <Grid item container wrap="nowrap" alignItems="center" spacing={3}>
+                    <Grid item container alignItems="center" spacing={2}>
                         <Grid item xs={12} sm={6}>
-                            <Typography sx={{fontSize: "90%"}}
-                                        variant="h6" gutterBottom
-                                        component="div">
+                            <Typography variant="subtitle1" gutterBottom>
                                 <strong>R&D Mode:</strong>
                             </Typography>
                         </Grid>
@@ -101,10 +86,7 @@ const NotificationsTab: FC = () => {
                                         disabled={loading}
                                     />
                                 }
-                                label={<Typography
-                                    sx={{fontSize: "90%"}}
-                                    variant="body1">Enable R&D
-                                    Mode</Typography>}
+                                label="Enable R&D Mode"
                             />
                         </Grid>
                     </Grid>
