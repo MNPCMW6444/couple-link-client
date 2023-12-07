8ccbfde1ed74d4342ea626345200a7acf7a54433
Michael Nusair <mnpcmw6444@gmail.com>
2023-12-07 10:48:51 +0200

    after sidebar cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import {Toaster} from "react-hot-toast";
 
 const serverURI = import.meta.env.VITE_NODE_ENV === "development" ? "://localhost:6005/graphql" : `s://${import.meta.env.VITE_WHITE_ENV === "preprod" ? "pre" : ""}server.couple-link.com/graphql`;
 console.log("import.meta.env.VITE_WHITE_ENV", import.meta.env.VITE_WHITE_ENV);
+console.log("import.meta.env", import.meta.env);
 const globalStyles = css`
   * {
     box-sizing: border-box;
